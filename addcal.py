b0101efe3e8204a6823e4a2e335b144335e90b8e
Henrique Vasconcelos <henriquetguedes@gmail.com>
2020-03-06 22:34:29 +0000

    addcal path

--- addcal.py
+++ addcal.py
@@ -7,6 +7,7 @@ from email.message import EmailMessage
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from pathlib import Path
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -17,18 +18,18 @@ def adiciona(nome, desc, dataini):
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('../credenciais/token.pickle'):
-        with open('../credenciais/token.pickle', 'rb') as token:
+    if os.path.exists(Path().resolve().parent / 'credenciais'/ 'token.pickle'):
+        with open(Path().resolve().parent / 'credenciais'/ 'token.pickle', 'rb') as token:
             creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('../credenciais/CMPcal.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(Path().resolve().parent / 'credenciais'/ 'CMPcal.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('../credenciais/token.pickle', 'wb') as token:
+        with open(Path().resolve().parent / 'credenciais'/ 'token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
