fce39c45655fc78bc22fe049ad3efe32e256bb68
Henrique Vasconcelos <henriquetguedes@gmail.com>
2022-11-07 16:17:49 +0000

    encoding 3

--- addcal.py
+++ addcal.py
@@ -3,8 +3,8 @@ from __future__ import print_function
 import datetime
 import pickle
 import os.path
-import smtplib
-from email.message import EmailMessage
+#import smtplib
+#from email.message import EmailMessage
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
